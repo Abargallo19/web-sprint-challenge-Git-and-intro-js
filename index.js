22bc8e2cfbe52075e888e108227ed119ca6049c3
Allesandria <mufasabugatti@Mufasas-MBP.lan>
2022-03-12 00:52:51 -0500

    task 7 completed

--- index.js
+++ index.js
@@ -262,7 +262,7 @@ Use removeArtist to do the following:
 5. Return the resulting copied array
 🌟 EXAMPLE: if removeArtist is invoked with the artists array and the number 0, it will return the resulting array with Amedeo Modigliani removed from our dataset. */
 
-let artistCopy = 9
+let artistCopy = 9;
 
 function removeArtist(array, index) {
   array.splice(array[index], 1);
@@ -287,8 +287,10 @@ Use addArtist to do the following:
 4. Return the array
 🌟 EXAMPLE: Invoking addArtist(artists, 'John Doe', '1988-2022', 'Full Stack Development', 'African American', 'I have a background in customer service at Big Retail Chain. I am attending BloomTech to become a Frontend Developer.') should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/) {
-  /*Your Code Here*/
+function addArtist(array) {
+const obj = {
+  
+}
 }
 
 
@@ -301,8 +303,14 @@ Use lotsOfArt to do the following:
 🌟 EXAMPLE: lotsOfArt(artists) will return ["Amedeo Modigliani", "Rene Magritte", ... "Albrecht Dürer"]
 */
 
-function lotsOfArt(/*Your Code Here*/) {
-  /*Your Code Here*/
+function lotsOfArt(array) {
+  const ohWhatArt = [];
+for(let i = 0 ; i < array.length; i++){
+  if(array[i].paintings > 100){
+    ohWhatArt.push(array[i].name)
+  }
+}
+return ohWhatArt;
 }
 
 
